5d44cc846eaca9158f640741968c91d772e0fd79
deedk822-lang <deedk822@gmail.com>
2025-11-03 18:45:13 +0200

    Add Jest configuration for ES modules and AI SDK testing

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  testMatch: ['**/test/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  setupFilesAfterEnv: [],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/index.js',
+    '!src/server.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'html'],
+  testTimeout: 30000, // 30 second timeout for AI provider tests
+  verbose: true
+};

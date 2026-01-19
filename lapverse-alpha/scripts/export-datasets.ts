39f9abbd3bd58db1b97b9fd1fa81bacf527343c8
Colab User <colab-user@example.com>
2026-01-19 01:26:41 +0000

    Cleaned up project files based on discussion.

--- lapverse-alpha/scripts/export-datasets.ts
+++
@@ -1,6 +0,0 @@
-// This script will be used to export datasets from Argilla.
-// It will be implemented in a future iteration.
-
-import { logger } from '../src/lib/logger';
-
-logger.info('Export datasets script placeholder.');
--- lapverse-core/src/delivery/OpenFeatureFlags.test.ts
+++
@@ -1,6 +0,0 @@
-import { OpenFeatureFlags } from './OpenFeatureFlags';
-
-test('global flag enabled', async ()=>{
-  const flags = new OpenFeatureFlags();
-  expect(await flags.isEnabled('task-v2', 'acme')).toBe(true);
-});
